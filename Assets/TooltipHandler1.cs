c28e8e58f6df79525df39eae5ed73187fb14036c
kookiepitz <qmsjsanchez1@tip.edu.ph>
2024-12-04 01:48:29 +0800

    initial commit

--- Assets/TooltipHandler1.cs
+++ Assets/TooltipHandler1.cs
@@ -1,26 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TooltipHandler1 : MonoBehaviour
 {
-    public GameObject tooltip; // Drag and drop the tooltip GameObject here in the Inspector
+    public GameObject tooltip; // The tooltip GameObject
+    private TextMeshPro textComponent; // Reference to the TextMeshPro component
+    private string[] tooltipMessages = { "Tooltip 1", "Tooltip 2", "Tooltip 3" }; // Example tooltip texts
+    private int currentIndex = 0; // Keeps track of the current tooltip message index
 
-    // Function to show the tooltip
+    void Start()
+    {
+        // Get the TextMeshPro component attached to the tooltip
+        if (tooltip != null)
+        {
+            textComponent = tooltip.GetComponent<TextMeshPro>();
+        }
+    }
+
+    // Show the tooltip when hovering over the cube
     public void ShowTooltip()
     {
         if (tooltip != null)
         {
-            tooltip.SetActive(true); // Activate the tooltip
+            tooltip.SetActive(true);
         }
     }
 
-    // Function to hide the tooltip
+    // Hide the tooltip when hover ends
     public void HideTooltip()
     {
         if (tooltip != null)
         {
-            tooltip.SetActive(false); // Deactivate the tooltip
+            tooltip.SetActive(false);
+        }
+    }
+
+    // Change the tooltip's text when clicked
+    public void UpdateTooltipText()
+    {
+        if (textComponent != null)
+        {
+            currentIndex = (currentIndex + 1) % tooltipMessages.Length; // Cycle through the tooltip messages
+            textComponent.text = tooltipMessages[currentIndex];
         }
     }
 }
+
