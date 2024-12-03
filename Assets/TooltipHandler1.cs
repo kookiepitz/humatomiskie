146980904f56269a08e50ed8097fdf5347d44ed6
kookiepitz <qmsjsanchez1@tip.edu.ph>
2024-12-04 02:09:32 +0800

    Revert "initial commit"
    This reverts commit c28e8e58f6df79525df39eae5ed73187fb14036c.

--- Assets/TooltipHandler1.cs
+++ Assets/TooltipHandler1.cs
@@ -1,50 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class TooltipHandler1 : MonoBehaviour
 {
-    public GameObject tooltip; // The tooltip GameObject
-    private TextMeshPro textComponent; // Reference to the TextMeshPro component
-    private string[] tooltipMessages = { "Tooltip 1", "Tooltip 2", "Tooltip 3" }; // Example tooltip texts
-    private int currentIndex = 0; // Keeps track of the current tooltip message index
+    public GameObject tooltip; // Drag and drop the tooltip GameObject here in the Inspector
 
-    void Start()
-    {
-        // Get the TextMeshPro component attached to the tooltip
-        if (tooltip != null)
-        {
-            textComponent = tooltip.GetComponent<TextMeshPro>();
-        }
-    }
-
-    // Show the tooltip when hovering over the cube
+    // Function to show the tooltip
     public void ShowTooltip()
     {
         if (tooltip != null)
         {
-            tooltip.SetActive(true);
+            tooltip.SetActive(true); // Activate the tooltip
         }
     }
 
-    // Hide the tooltip when hover ends
+    // Function to hide the tooltip
     public void HideTooltip()
     {
         if (tooltip != null)
         {
-            tooltip.SetActive(false);
-        }
-    }
-
-    // Change the tooltip's text when clicked
-    public void UpdateTooltipText()
-    {
-        if (textComponent != null)
-        {
-            currentIndex = (currentIndex + 1) % tooltipMessages.Length; // Cycle through the tooltip messages
-            textComponent.text = tooltipMessages[currentIndex];
+            tooltip.SetActive(false); // Deactivate the tooltip
         }
     }
 }
-
